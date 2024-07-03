ec91fda90fd5850e9b1a9907a2b4c0322dcc5e0c
ajfahim <ajfahim52@gmail.com>
2024-07-03 21:42:59 +0600

    initial

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import app from "./app.js";
+import { config } from "dotenv";
+import { connectToDatabase } from "./db/connection.js";
+
+config();
+
+//connections and listeneres
+const PORT = process.env.PORT || 5000;
+connectToDatabase()
+  .then(() => {
+    app.listen(PORT, () =>
+      console.log("Server Open & Connected To Database 🤟")
+    );
+  })
+  .catch((err) => console.log(err));
