3817570548f59226ff56e763ef825acc259cd4f2
ajfahim <ajfahim52@gmail.com>
2024-07-03 23:10:38 +0600

    cors

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,22 @@
-import express from "express";
-import { config } from "dotenv";
-import morgan from "morgan";
-import appRouter from "./routes/index.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import { config } from "dotenv";
+import express from "express";
+import morgan from "morgan";
+import appRouter from "./routes/index.js";
 config();
 const app = express();
 
 //middlewares
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://chat-ai-backend-khaki.vercel.app",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
